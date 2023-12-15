b202a0cb90711f92d86f6a82af13a3ae06a09270
Gabriel Poeta <gugapoeta@gmail.com>
2023-12-15 01:33:06 -0500

    Update Dropdown component

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -10,7 +10,7 @@ export function Dropdown ({ onPriceChange, onPricePer100gChange, onLowestPricePe
 
   useEffect(() => {
     // Fetch data from your API endpoint or route
-    fetch(`/list/`)
+    fetch(`server/list/`)
       .then((response) => response.json())
       .then((data) => setDocuments(data))
       .catch((error) => console.error("Error fetching data:", error));
