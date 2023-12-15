2c48fb6f50d94bde6f364b63b850b4b065fadbfc
Gabriel Poeta <gugapoeta@gmail.com>
2023-12-15 01:18:06 -0500

    Update cors function

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const corsOptions = {
   optionsSuccessStatus: 204,
 };
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 const port = process.env.PORT || 5000;
 const HOST = process.env.HOST || '0.0.0.0';
