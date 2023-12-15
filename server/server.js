77dca2ede7795c1f7af08785fa7c02982816144d
Gabriel Poeta <gugapoeta@gmail.com>
2023-12-15 01:02:53 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,17 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "../.env" });
 const port = process.env.PORT || 5000;
+
+
+const { MongoClient } = require("mongodb");
+const Db = process.env.MONGODB_URI;
+const client = new MongoClient(Db, {
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
+});
+ 
+var _db;
+
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
@@ -16,3 +27,22 @@ app.listen(port, async () => {
   });
   console.log(`Server is running on port: ${port}`);
 });
+
+ 
+module.exports = {
+    connectToServer: async function (callback) {
+  
+      try {
+        await client.connect();
+      } catch (e) {
+        console.error(e);
+      }
+  
+      _db = client.db("foodBasket");
+  
+      return (_db === undefined ? false : true);
+    },
+    getDb: function () {
+      return _db;
+    },
+  };
