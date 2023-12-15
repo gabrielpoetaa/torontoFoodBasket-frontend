c1b36d486b0b409a0368fb0f9dff75b674c9a695
Gabriel Poeta <gugapoeta@gmail.com>
2023-12-15 01:16:01 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,16 +3,25 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "../.env" });
 
+
+const corsOptions = {
+  origin: 'https://toronto-food-basket-ezf18bsfl-gabriels-projects-ce63e102.vercel.app/', // Replace with your Vercel app domain
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
 app.use(cors());
 
 const port = process.env.PORT || 5000;
+const HOST = process.env.HOST || '0.0.0.0';
 
 app.use(express.json());
 app.use(require("./routes/record"));
 // get driver connection
 const dbo = require("./db/conn");
 
-app.listen(port, async () => {
+app.listen(port, HOST, async () => {
   // perform a database connection when server starts
   await dbo.connectToServer(function (err) {
     if (err) console.error(err);
