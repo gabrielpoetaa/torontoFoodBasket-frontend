0b404a6367a3f56e058e598c3042edeab08ce7b3
Gabriel Poeta <gugapoeta@gmail.com>
2024-03-07 18:35:57 -0500

    add:logo and favicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 // Header.js
 import React from "react";
-import styles from "./Header.module.css";
+import styles from "./Header.module.css"; 
+import logo from "../images/foodBasketLogo-white.png"
 
-export function Header({ }) {
+export function Header() {
   return (
     <div className={styles.headerContainer}>
       <div className={styles.headerText}>
-        <h1>TORONTO FOOD BASKET</h1>
+        <div className={styles.logo}>
+        <img src={logo} alt="Toronto Food Basket logo" />
+        </div>
       </div>
     </div>
   );
