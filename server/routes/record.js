b1084500b124f98bf2feca22d51d9a16e0466ba5
GabrielMoraes <gugapoeta@gmail.com>
2024-05-18 14:40:02 -0400

    del: Chicken Drumstick from list

--- server/routes/record.js
+++ server/routes/record.js
@@ -21,7 +21,7 @@ recordRoutes.route("/").get(async function (req, res) {
     // Query Meat Department Collection
     const meatDepartmentsCollection = db_connect.collection("meatdepartments"); // important!!!
     const resultMeatDepartmens = await meatDepartmentsCollection
-    .find({ titulo: { $ne: "Chicken Drumstick" } })
+    .find({ title: { $ne: "Chicken Drumstick" } })
     .toArray();
 
     // Query Bakery Department Collection
