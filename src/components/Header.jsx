947041464c6c07b0d41713f6087556b62cc3e951
Gabriel Poeta <95362336+gabrielpoetaa@users.noreply.github.com>
2025-04-20 19:53:07 -0400

    resize main logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 // Header.js
 import React from "react";
-import styles from "./Header.module.css"; 
-import logo from "../images/foodBasketLogo-white.png"
+import styles from "./Header.module.css";
+import logo from "../images/foodBasketLogo-white.png";
 
 export function Header() {
   return (
     <div className={styles.headerContainer}>
       <div className={styles.headerText}>
         <div className={styles.logo}>
-        <img src={logo} alt="Toronto Food Basket logo" />
+          <img src={logo} alt="Toronto Food Basket logo" />
         </div>
       </div>
     </div>
